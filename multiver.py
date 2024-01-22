881a44a22bf373145f96c1d91223f0770652ebe1
tabledrop <79778654+tabledrop@users.noreply.github.com>
2024-01-22 02:16:28 -0600

    complete rewrite
    heavily needed for linux testing

--- multiver.py
+++ multiver.py
@@ -1,76 +1,31 @@
-import windowgen
+from tkinter import *
+from ttkbootstrap.constants import *
+import ttkbootstrap as tb 
 import platform
+import distro
 
-PLATFORM_OS_NAME = platform.system()
-
-def DisplaymacOS():
+# simply gathers OS name, version, and includes Linux distro guesser
+def compileOSInfo():
     result = []
-
-    if PLATFORM_OS_NAME != "Darwin":
-        print("[DEBUG]: Not macOS")
-
-    # if somehow you have a non-standard macOS version
-    # (impossible unless you're a macOS dev)
-    # this is a generic placeholder for that
-    DarwinMajorVersion = 0
-    DarwinVersion = "Generic"
-    result.append(DarwinMajorVersion)
-    result.append(DarwinVersion)
-
-    if PLATFORM_OS_NAME == "Darwin":
-        if int(float(platform.release()[0:3])) == 0:
-            return result
+    result.append(platform.system()) # OS name
+    if platform.system() == "Darwin":
+        result.append(int(float(platform.release()[0:3]))
+    elif platform.system() == "Linux":
+        if distro.os_release_attr("version") == '':
+            result.append("rolling")
         else:
-            result = []
-            DarwinVersion = platform.release()
-            DarwinMajorVersion = int(float(platform.release()[0:3]))
-            result.append(DarwinMajorVersion)
-            result.append(DarwinVersion)
-            return result
+            result.append(distro.os_release_attr("version"))
+    return result
 
-def DisplayWindows():
-    result = []
+print(compileOSInfo())
 
-    if PLATFORM_OS_NAME != "Windows":
-        print("[DEBUG]: Not Windows")
+# window init
+root = tb.Window(themename="cosmo")
+root.title("multiver.py")
+root.geometry('400x450')
 
-    WindowsMajorVersion = 5.1
-    WindowsVersion = "XP"
-    result.append(WindowsMajorVersion)
-    result.append(WindowsVersion)
+exitButton = tb.Button(text="OK",
+                       bootstyle="default, outline")
+exitButton.place(x=330,y=400)
 
-    if PLATFORM_OS_NAME == "Windows":
-        if platform.release() == 5.1:
-            return result
-        else:
-            result = []
-            WindowsMajorVersion = platform.release()
-            WindowsVersion = platform.release()
-            result.append(WindowsMajorVersion)
-            result.append(WindowsVersion)
-            return result
-
-def DisplayLinux():
-    result = []
-
-    if PLATFORM_OS_NAME != "Linux":
-        print("[DEBUG]: Not Linux")
-
-    if PLATFORM_OS_NAME == "Linux":
-        print("[DEBUG]: TO BE IMPLEMENTED")
-
-print(DisplaymacOS())
-print(DisplayWindows())
-print(DisplayLinux())
-
-# def PlatformDiscover():
-#    if PLATFORM_OS_NAME == "Windows":
-#        print("Winver exists lol")
-#   elif PLATFORM_OS_NAME == "Darwin":
-#        DarwinMajorVer = int(float(platform.release()[0:3]))
-#       DarwinVersion = platform.release()
-#        print(DarwinVersion)
-#    elif PLATFORM_OS_NAME == "Linux":
-#        print("literal chad lol")
-
-# PlatformDiscover()
+root.mainloop()
